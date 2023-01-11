b0106bffd358b9340839220e824d2fdb0635ab51
Uchenna Iwuchukwu Oreoluwa <64827412+Uchennaore@users.noreply.github.com>
2023-01-11 05:06:04 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
+"""Makes a list from args and saves them to a file in json format"""
+import sys
 import json
-from sys import argv
-"""
-This file contains a function that adds
-all arguments to a python list and saves
-to a file
-"""
-
-
-save_json = __import__('7-save_to_json_file').save_to_json_file
-load_json = __import__('8-load_from_json_file').load_from_json_file
-
-filename = 'add_item.json'
+savejson = __import__("7-save_to_json_file").save_to_json_file
+loadjson = __import__("8-load_from_json_file").load_from_json_file
 
 try:
-    new = load_json(filename)
-except (ValueError, FileNotFoundError):
-    new = []
-
-new = new + argv[1:]
-save_json(new, filename)
+    listy = loadjson("add_item.json")
+except FileNotFoundError:
+    listy = []
+for arg in sys.argv[1:]:
+    listy.append(arg)
+savejson(listy, "add_item.json")
