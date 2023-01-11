62c6c54b67176fa4a29b7ed17e1b7f5ecd271464
Uchenna Iwuchukwu Oreoluwa <64827412+Uchennaore@users.noreply.github.com>
2023-01-11 04:57:33 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-import json
 """
-This file contains a function that
-writes an obj to a text file using
-JSON rep
+Adding all arguments to a Python list and saving them to a file.
 """
 
+import os.path
+import sys
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-def save_to_json_file(my_obj, filename):
-    """
-    function that writes an obj  to a text file
-    """
-    with open(filename, mode="w") as myFile:
-        json.dump(my_obj, myFile)
+my_file = 'add_item.json'
+if (os.path.isfile(my_file)):
+    my_obj = load_from_json_file(my_file)
+else:
+    my_obj = []
+my_obj.extend(sys.argv[1:])
+save_to_json_file(my_obj, my_file)
