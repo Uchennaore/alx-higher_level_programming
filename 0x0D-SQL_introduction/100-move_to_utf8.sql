38d6e1cddc909b79b9129722e37bfa97411529d1
Uchenna Iwuchukwu Oreoluwa <64827412+Uchennaore@users.noreply.github.com>
2023-02-15 09:50:20 +0100

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,8 @@
--- script that converts database to UTF-8
+-- convert database to UTF8
+-- convert database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE hbtn_0c_0;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+-- select hbtn_0c_0 database
+USE hbtn_0c_0
+-- convert first_table to UTF8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
