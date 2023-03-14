b08525bc3c44b5f1288757fc1da4514068a1b463
Uchenna Iwuchukwu Oreoluwa <64827412+Uchennaore@users.noreply.github.com>
2023-03-14 01:48:10 +0100

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const value of array) {
-  console.log(value);
+let arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let length = arr.length;
+let i = 0;
+
+for (i = 0; i < length; i++) {
+  console.log(arr[i]);
 }
