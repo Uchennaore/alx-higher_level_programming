f801a22d705b4c999ef4735286190fe715c787fa
Uchenna Iwuchukwu Oreoluwa <64827412+Uchennaore@users.noreply.github.com>
2023-03-14 01:21:23 +0100

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,7 @@
 #!/usr/bin/node
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+
+const myMessages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i;
+for (i = 0; i < myMessages.length; i++) {
+  console.log(myMessages[i]);
+}
