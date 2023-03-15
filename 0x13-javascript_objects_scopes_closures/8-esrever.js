6e742006d6333cf10b67873d6255e81fbc6e935f
Uchenna Iwuchukwu Oreoluwa <64827412+Uchennaore@users.noreply.github.com>
2023-03-15 01:21:51 +0100

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-
-module.exports.esrever = function (list) {
-  let newlist = [];
-  let c = 0;
-  for (let l = list.length - 1; l >= 0; l--, c++) {
-    newlist[c] = list[l];
-  }
-  return newlist;
+exports.esrever = function (list) {
+  return list.reduceRight(function (array, current) {
+    array.push(current);
+    return array;
+  }, []);
 };
