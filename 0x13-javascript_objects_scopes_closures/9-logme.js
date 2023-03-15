66dd38e9901b18d7a242b5573f476685cdfe397b
Uchenna Iwuchukwu Oreoluwa <64827412+Uchennaore@users.noreply.github.com>
2023-03-15 01:03:18 +0100

    Update 9-logme.js

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-exports.esrever = function (list) {
-  let reverse = [];
-  let i = list.length - 1;
-  while (i >= 0) {
-    reverse.push(list[i]);
-    i--;
-  }
-  return reverse;
+
+module.exports.logMe = function (item) {
+  console.log(`${module.exports.logMe.count}: ${item}`);
+  module.exports.logMe.count++;
 };
+
+module.exports.logMe.count = 0;
