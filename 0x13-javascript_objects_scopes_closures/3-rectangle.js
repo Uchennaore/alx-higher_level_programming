11f8f9ad8065f9a5495e5c9f6c36ca76579605f3
Uchenna Iwuchukwu Oreoluwa <64827412+Uchennaore@users.noreply.github.com>
2023-03-15 01:11:56 +0100

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -12,4 +12,13 @@ module.exports = class Rectangle {
       console.log('X'.repeat(this.width));
     }
   }
+  rotate () {
+    let temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
 };
