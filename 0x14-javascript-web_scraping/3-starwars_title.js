be91ac6edcd4b08d2b559b6f57afcd60d8a3dc98
Uchenna Iwuchukwu Oreoluwa <64827412+Uchennaore@users.noreply.github.com>
2023-04-26 02:32:34 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-// makes get request for SW movie id
 const request = require('request');
-request(`http://swapi.co/api/films/${process.argv[2]}`, function (error, response, body) {
-  error && console.log(error);
-  console.log(JSON.parse(body).title);
+request.get('http://swapi.co/api/films/' + process.argv[2] + '/', function (err, response, body) {
+  if (err) throw err;
+  else if (response.statusCode === 200) {
+    console.log(JSON.parse(body).title);
+  }
 });
